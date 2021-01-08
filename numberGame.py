41f5d41de5dd334e135da15a54508173ccf324f4
Snigdha_13 <65667637+Snigdha-Pothuri@users.noreply.github.com>
2021-01-08 21:43:55 +0530

    Add files via upload

--- numberGame.py
+++ numberGame.py
@@ -1,6 +1,7 @@
 import random
 number=random.randint(1,9)
-chance=5
+chance=0
+while chance<5:
  guess=int(input("Enter a number between 1 and 10 : "))
  if(guess==number):
     print("Congratulations You Won :)")
@@ -8,5 +9,6 @@ chance=5
     print("Wrong :( Try a bigger number")
  elif(guess>number):
     print("Wrong :( Try a smaller number") 
+ chance=chance+1
 if not chance<5:
-    print("You Lose the number is :",number)
+ print("You Lose the number is :",number)
